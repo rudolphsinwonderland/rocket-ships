992a61ef3e35ae0dd096769deffbd61b108616a1
vartdader <ahmettanrikulu24@gmail.com>
2021-12-13 12:54:00 +0300

    dasd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="flex items-center justify-center relative w-full h-60   ">
         <StyledHeader className="text-5xl lg:text-7xl ">
-          SOLANA BLUE CHIP ROCKET SHIP
+          SOLANA BLUE CHIP ROCKET SHIP sds
         </StyledHeader>
       </nav>
     </div>
