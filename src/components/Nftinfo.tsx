d2fd5dc182b6562a7e59bd2a6ad1fdc25dc1d98c
vartdader <ahmettanrikulu24@gmail.com>
2021-12-13 12:37:35 +0300

    sds

--- src/components/Nftinfo.tsx
+++ src/components/Nftinfo.tsx
@@ -6,14 +6,10 @@ function Nftinfo() {
       <div className="items-center text-center mt-20 space-y-2">
         <h1 className="text-5xl underline">NFTOKENOMICS</h1>
         <p className="text-xl">1111 TOTAL SUPPLY</p>
-        <p className="text-xl">0.10 SOL MINT PRICE</p>
-        <p className="text-xl">ROYALITIES 6.9%</p>
+        <p className="text-xl">0.5 SOL MINT PRICE</p>
+        <p className="text-xl">ROYALITIES 5%</p>
+        <p className="text-xl">$FIN TOKEN AIRDROP</p>
         <p className="text-xl">1500x500 px</p>
-        <p className="text-xl">100 TRAITS</p>
-      </div>
-
-      <div className="p-12 text-3xl">
-        NO TOKEN, NO ROADMAP, NO UTILITY. just a rocket ship
       </div>
     </div>
   );
