85849a9c9800565fde54463048de8fa7dc46c934
vartdader <ahmettanrikulu24@gmail.com>
2021-12-16 23:18:25 +0300

    fix prize

--- src/components/Rewards.tsx
+++ src/components/Rewards.tsx
@@ -5,7 +5,9 @@ const Rewards = () => {
     <div>
       <div className="pb-32">
         <div className="items-center text-center mt-20 space-y-2">
-          <StyledHone className="text-5xl underline">REWARDS</StyledHone>
+          <StyledHone className="text-5xl underline">
+            PRIZES UP TO 300 SOL
+          </StyledHone>
           <StyledP className="text-xl">Winner ticket 40% of pool</StyledP>
           <StyledP className="text-xl">Second big winner 20%</StyledP>
           <StyledP className="text-xl">3rd & 4th winner 5%</StyledP>
